250b210f37b6b6025e448f3ae6a209e41b467159
who <114432362+who2101@users.noreply.github.com>
2022-10-08 23:56:45 +0500

    Added choose type of calculate

--- calc mass fraction.cpp
+++ calc mass fraction.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main()
 {
+    int type;
     string arg, arg2;
-  
+
+    cout << "Write type of calculate mass (1 or 2)" << endl;
+    cin >> type;
+
     cout << "Write first argument" << endl;
     cin >> arg;
     
@@ -14,12 +18,12 @@ int main()
 
     if(arg[0] != '\0' || arg2[0] != '\0')
     {
-        float iArg = stof(arg); float iArg2 = stof(arg2);
+        float fArg = stof(arg); float fArg2 = stof(arg2);
         
-        if(iArg != 0.0 || iArg2 != 0.0)
+        if(fArg != 0.0 || fArg2 != 0.0)
         {
-            cout << "Result: " << ((iArg2/(iArg+iArg2))*100) << endl;
-            cout << "Result: " << ((iArg*iArg2)/100) << endl;
+            if(type == 1) cout << "Result: " << ((fArg2/(fArg+fArg2))*100) << endl;
+            else if(type == 2) cout << "Result: " << ((fArg*fArg2)/100) << endl;
         }
     }
 
