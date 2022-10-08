70a933f8fae877a4fe30d6175d04195eaba0287c
who <114432362+who2101@users.noreply.github.com>
2022-10-08 23:59:14 +0500

    Deleted check if(string != null)

--- calc mass fraction.cpp
+++ calc mass fraction.cpp
@@ -16,15 +16,12 @@ int main()
     cout << "Write second argument" << endl;
     cin >> arg2;
 
-    if(arg[0] != '\0' || arg2[0] != '\0')
-    {
-        float fArg = stof(arg); float fArg2 = stof(arg2);
+    float fArg = stof(arg); float fArg2 = stof(arg2);
         
-        if(fArg != 0.0 || fArg2 != 0.0)
-        {
-            if(type == 1) cout << "Result: " << ((fArg2/(fArg+fArg2))*100) << endl;
-            else if(type == 2) cout << "Result: " << ((fArg*fArg2)/100) << endl;
-        }
+    if(fArg != 0.0 || fArg2 != 0.0)
+    {
+        if(type == 1) cout << "Result: " << ((fArg2/(fArg+fArg2))*100) << endl;
+        else if(type == 2) cout << "Result: " << ((fArg*fArg2)/100) << endl;
     }
 
     return 0;
