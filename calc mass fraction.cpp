bc55b5be7e351938932743399fc138ecc96516d5
who <114432362+who2101@users.noreply.github.com>
2022-10-09 12:54:27 +0500

    Update calc mass fraction.cpp

--- calc mass fraction.cpp
+++ calc mass fraction.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main()
 {
-    int type;
-    string arg, arg2;
+    int type; float fArg, fArg2; string arg, arg2;
 
     cout << "Write type of calculate mass (1 or 2)" << endl;
     cin >> type;
@@ -16,7 +15,7 @@ int main()
     cout << "Write second argument" << endl;
     cin >> arg2;
 
-    float fArg = stof(arg); float fArg2 = stof(arg2);
+    fArg = stof(arg); fArg2 = stof(arg2);
         
     if(fArg != 0.0 || fArg2 != 0.0)
     {
