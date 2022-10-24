6e97f088788e0d56ec0a884eab1e02c0633d5af1
who <114432362+who2101@users.noreply.github.com>
2022-10-24 18:29:31 +0300

    Update calc mass fraction.cpp

--- calc mass fraction.cpp
+++ calc mass fraction.cpp
@@ -2,25 +2,27 @@
 
 int main()
 {
-    int type; float fArg, fArg2; string arg, arg2;
+    int type; 
+    float fArg, fArg2; 
+    std::string arg, arg2;
 
-    cout << "Write type of calculate mass (1 or 2)" << endl;
-    cin >> type;
+    std::cout << "Write type of calculate mass (1 or 2)" << std::endl;
+    std::cin >> type;
 
-    cout << "Write first argument" << endl;
-    cin >> arg;
+    std::cout << "Write first argument" << std::endl;
+    std::cin >> arg;
     
-    cout << "Write second argument" << endl;
-    cin >> arg2;
+    std::cout << "Write second argument" << std::endl;
+    std::cin >> arg2;
 
     fArg = stof(arg); fArg2 = stof(arg2);
         
     if(fArg && fArg2 != 0.0)
     {
-        if(type == 1) cout << "Result: " << ((fArg2/(fArg+fArg2))*100) << endl;
-        else if(type == 2) cout << "Result: " << ((fArg*fArg2)/100) << endl;
+        if(type == 1) std::cout << "Result: " << ((fArg2/(fArg+fArg2))*100) << std::endl;
+        else if(type == 2) std::cout << "Result: " << ((fArg*fArg2)/100) << std::endl;
     }
-    else cout << "ERROR: Одно из чисел равно нулю!" << endl;
+    else std::cout << "ERROR: Одно из чисел равно нулю!" << std::endl;
 
     return 0;
 }
