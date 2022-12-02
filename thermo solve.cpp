62f4c5a128776c49bbbbef8ef0d203be182375f1
who <114432362+who2101@users.noreply.github.com>
2022-12-02 11:20:41 +0500

    Code improvement
    Add "using namespace std" and improve if statement

--- thermo solve.cpp
+++ thermo solve.cpp
@@ -1,59 +1,63 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
-    std::string arg, arg2, arg3, arg4;
+    string arg, arg2, arg3, arg4;
     float fArg, fArg2, fArg3, fArg4;
 	
-    std::cout << "Введите массу вещества" << std::endl;
-    std::cout << std::endl;
-    std::cin >> arg; // mass
-    std::cout << std::endl;
+    cout << "Введите массу вещества" << endl;
+    cout << endl;
+    cin >> arg;
+    scout << endl;
 
-    std::cout << "Введите молекулярную массу" << std::endl;
-    std::cout << std::endl;
-    std::cin >> arg2; // mr
-    std::cout << std::endl;
+    cout << "Введите молекулярную массу" << endl;
+    cout << endl;
+    cin >> arg2;
+    cout << endl;
 	
-    std::cout << "Введите кол-во kJ" << std::endl;
-    std::cout << std::endl;
-    std::cin >> arg3; //kj
-    std::cout << std::endl;
+    cout << "Введите кол-во kJ" << endl;
+    cout << endl;
+    cin >> arg3;
+    cout << endl;
     
-    std::cout << "Введите кол-во моль" << std::endl;
-    std::cout << std::endl;
-    std::cin >> arg4; // mole
-    std::cout << std::endl;
+    cout << "Введите кол-во моль" << endl;
+    cout << endl;
+    cin >> arg4;
+    cout << endl;
 	
     fArg = stof(arg);
     fArg2 = stof(arg2);
     fArg3 = stof(arg3);
     fArg4 = stof(arg4);
 	
-    if(fArg || fArg2 || fArg3 || fArg4 != 0.0)
+    if(fArg || fArg2 || fArg3 || fArg4 <= 0.0)
     {
-        std::cout << std::endl;
-	
-        std::cout << std::endl;
-	std::cout << "Дано:" << std::endl;
-	std::cout << std::endl;
-	    
-	std::cout << "m (масса вещества) = " << fArg << std::endl;
-	std::cout << "Mr (Молекулярная масса = " << fArg2 << std::endl;
-	std::cout << "kJ (кдж) = " << fArg3 << std::endl;
-	std::cout << "Кол-во моль: " << fArg4 << std::endl;
-	std::cout << std::endl;
-	    
-	std::cout << "Решение:" << std::endl;
-	std::cout << std::endl;
-        std::cout << "n = " << fArg << "/" << fArg2 << " = " << fArg/fArg2 << " гр/моль" << std::endl;
-	std::cout << "x = " <<  "(" << fArg/fArg2 << " гр/моль" << " * " << fArg3 << " kJ" << ")" << " / " << fArg4 << " моль " << " = " << fArg/fArg2*fArg3/fArg4 << " kJ" << std::endl;
-	    
-	std::cout << std::endl;
-	std::cout << "Ответ: " << fArg/fArg2*fArg3/fArg4 << " kJ" << std::endl;
+         cout << "ERROR: Какой-то из аргументов меньше или равен 0" << endl;
+
+         return 0;
     }
-    else std::cout << "ERROR: Какое то число равно нулю!" << std::endl;
+
+    cout << endl;
+    cout << "Дано:" << endl;
+    cout << endl
+    cout << endl
+
+    cout << "m (масса вещества) = " << fArg << endl;
+    cout << "Mr (Молекулярная масса = " << fArg2 << endl;
+    cout << "kJ (кдж) = " << fArg3 << endl;
+    cout << "Кол-во моль: " << fArg4 << endl;
+    cout << endl;
+	    
+    cout << "Решение:" << endl;
+    cout << endl;
+    cout << "n = " << fArg << "/" << fArg2 << " = " << fArg/fArg2 << " гр/моль" << endl;
+    cout << "x = " <<  "(" << fArg/fArg2 << " гр/моль" << " * " << fArg3 << " kJ" << ")" << " / " << fArg4 << " моль " << " = " << fArg/fArg2*fArg3/fArg4 << " kJ" << endl;
+	    
+    cout << endl;
+    cout << "Ответ: " << fArg/fArg2*fArg3/fArg4 << " kJ" << endl;
 	
     return 0;
 }
